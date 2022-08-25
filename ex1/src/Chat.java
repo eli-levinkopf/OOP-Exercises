2605a230e78eecc9dba16ccf592dabf6cd19e050
elilevinkopf <eli.levinkopf@mail.huji.ac.il>
2022-08-25 14:21:45 +0300

    Tests passed

--- ex1/src/Chat.java
+++ ex1/src/Chat.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 
-class Chat{
-    public static void main(String[] args){
-        String[] repliesToIllegalRequestBot1 = {"what", "say should I say"};
-        String[] repliesToIllegalRequestBot2 = {"whaaat", "say say"};
-        ChatterBot[] bots = {new ChatterBot(repliesToIllegalRequestBot1), new ChatterBot(repliesToIllegalRequestBot2)};
-        Scanner scanner = new Scanner(System.in);
-        String statement = "Hello";
+class Chat {
+    static final String STATEMENT_TO_START = "Hello";
 
-        for (int i = 0; ; ++i){
-            statement = bots[i% bots.length].replyTo(statement);
-            System.out.print(statement);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String statement = STATEMENT_TO_START;
+        String[] repliesToIllegalRequestBot1 = {"say what? " + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER + "? what’s "
+                + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER + "?", "whaaat??", "Now I'm tired! So I don't understand you",
+                "I don't know how to say " + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER + ", try something else"};
+        String[] repliesToIllegalRequestBot2 = {"I don't want to say " + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER,
+                "I don't like to say " + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER + ", but for you I say this: "
+                        + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER, "whaaat??"};
+        String[] repliesToLegalRequestBot1 = {"say " + ChatterBot.REQUESTED_PHRASE_PLACEHOLDER + "? okay: " +
+                ChatterBot.REQUESTED_PHRASE_PLACEHOLDER, "okay, here goes: " + ChatterBot.REQUESTED_PHRASE_PLACEHOLDER};
+        String[] repliesToLegalRequestBot2 = {ChatterBot.REQUESTED_PHRASE_PLACEHOLDER + " is my favorite thing to say. Here: "
+                + ChatterBot.REQUESTED_PHRASE_PLACEHOLDER, "You want me to say " + ChatterBot.REQUESTED_PHRASE_PLACEHOLDER
+                + ", do you? alright: " + ChatterBot.REQUESTED_PHRASE_PLACEHOLDER};
+
+        ChatterBot[] bots = {new ChatterBot("Bot1", repliesToLegalRequestBot1, repliesToIllegalRequestBot1),
+                new ChatterBot("Bot2", repliesToLegalRequestBot2, repliesToIllegalRequestBot2)};
+
+        for (int i = 0; ; ++i) {
+            statement = bots[i % bots.length].replyTo(statement);
+            System.out.print(bots[i % bots.length].getName() + ": " + statement);
             scanner.nextLine();
         }
     }
--- ex1/src/ChatterBot.java
+++ ex1/src/ChatterBot.java
@@ -1,43 +1,37 @@
-
 import java.util.*;
 
-/**
- * Base file for the ChatterBot exercise.
- * The bot's replyTo method receives a statement.
- * If it starts with the constant REQUEST_PREFIX, the bot returns
- * whatever is after this prefix. Otherwise, it returns one of
- * a few possible replies as supplied to it via its constructor.
- * In this case, it may also include the statement after
- * the selected reply (coin toss).
- * @author Dan Nirel
- */
 class ChatterBot {
     static final String REQUEST_PREFIX = "say ";
+    static final String REQUESTED_PHRASE_PLACEHOLDER = "<phrase>";
+    static final String ILLEGAL_REQUEST_PLACEHOLDER = "<request>";
 
     Random rand = new Random();
-    String[] repliesToIllegalRequest;
+    String[] repliesToIllegalRequest, repliesToLegalRequest;
+    String name;
 
-    ChatterBot(String[] repliesToIllegalRequest) {
+    ChatterBot(String name, String[] repliesToLegalRequest, String[] repliesToIllegalRequest) {
+        this.name = name;
+        this.repliesToLegalRequest = new String[repliesToLegalRequest.length];
         this.repliesToIllegalRequest = new String[repliesToIllegalRequest.length];
-        for(int i = 0 ; i < repliesToIllegalRequest.length ; i = i+1) {
-            this.repliesToIllegalRequest[i] = repliesToIllegalRequest[i];
-        }
+
+        System.arraycopy(repliesToLegalRequest, 0, this.repliesToLegalRequest, 0, repliesToLegalRequest.length);
+        System.arraycopy(repliesToIllegalRequest, 0, this.repliesToIllegalRequest, 0, repliesToIllegalRequest.length);
     }
 
     String replyTo(String statement) {
-        if(statement.startsWith(REQUEST_PREFIX)) {
-            //we don’t repeat the request prefix, so delete it from the reply
-            return statement.replaceFirst(REQUEST_PREFIX, "");
+        if (statement.startsWith(REQUEST_PREFIX)) {
+            return replacePlaceholderInARandomPattern(repliesToLegalRequest, REQUESTED_PHRASE_PLACEHOLDER, statement.replaceFirst(REQUEST_PREFIX, ""));
         }
-        return respondToIllegalRequest(statement);
+        return replacePlaceholderInARandomPattern(repliesToIllegalRequest, ILLEGAL_REQUEST_PLACEHOLDER, statement);
     }
 
-    String respondToIllegalRequest(String statement) {
-        int randomIndex = rand.nextInt(repliesToIllegalRequest.length);
-        String reply = repliesToIllegalRequest[randomIndex];
-        if(rand.nextBoolean()) {
-            reply = reply + statement;
-        }
-        return reply;
+    String replacePlaceholderInARandomPattern(String[] possibleResponses, String placeholder, String replaceTo) {
+        int randomIndex = rand.nextInt(possibleResponses.length);
+        String responsePattern = possibleResponses[randomIndex];
+        return responsePattern.replaceAll(placeholder, replaceTo);
+    }
+
+    String getName() {
+        return name;
     }
 }
