d46163c09c2044e89dd1fe49283f9bcdcf39d48a
elilevinkopf <eli.levinkopf@mail.huji.ac.il>
2022-10-27 20:25:58 +0300

    BrightnessImgCharMatcher

--- ex4_1/src/ascii_art/Driver.java
+++ ex4_1/src/ascii_art/Driver.java
@@ -1,10 +1,16 @@
 package ascii_art;
 
+import ascii_art.img_to_char.BrightnessImgCharMatcher;
 import ascii_art.img_to_char.CharRenderer;
+
+import java.util.Arrays;
 
 public class Driver {
     public static void main(String[] args){
-        boolean[][] cs = CharRenderer.getImg('c', 16, "Ariel");
-        CharRenderer.printBoolArr(cs);
+//        boolean[][] cs = CharRenderer.getImg('c', 16, "Ariel");
+//        CharRenderer.printBoolArr(cs);
+        BrightnessImgCharMatcher brightnessImgCharMatcher = new BrightnessImgCharMatcher();
+        brightnessImgCharMatcher.helper();
+
     }
 }
--- ex4_1/src/ascii_art/img_to_char/BrightnessImgCharMatcher.java
+++ ex4_1/src/ascii_art/img_to_char/BrightnessImgCharMatcher.java
@@ -1,19 +1,41 @@
 package ascii_art.img_to_char;
 
+import java.util.Arrays;
+
 public class BrightnessImgCharMatcher {
 
+    public static final int NUM_OF_PIXELS_FOR_CHAR = 16;
 
-    BrightnessImgCharMatcher(){
-
-    }
-
-    public char[][] chooseChars(int numCharsInRow, Character[] charSet){
+    public char[][] chooseChars(int numCharsInRow, Character[] charSet) {
         return null;
     }
 
-    private float getBrightness(char c){
-        boolean[][] booleanMatrix = CharRenderer.getImg(c, 16, "Ariel");
-        for ()
-        return .1f;
+    private float[] getBrightness(char[] charsArray) {
+        float[] brightnessArray = new float[charsArray.length];
+        for (int i = 0; i < brightnessArray.length; i++) {
+            brightnessArray[i] = BrightnessImgChar(charsArray[i]);
+        }
+        return brightnessArray;
     }
+
+    private float BrightnessImgChar(char c) {
+        boolean[][] booleanMatrix = CharRenderer.getImg(c, NUM_OF_PIXELS_FOR_CHAR, "Ariel");
+//        CharRenderer.printBoolArr(booleanMatrix);
+        int count = 0;
+        for (boolean[] row : booleanMatrix) {
+            for (boolean bool : row) {
+                if (bool) {
+                    ++count;
+                }
+            }
+        }
+        return count / (float) Math.pow(NUM_OF_PIXELS_FOR_CHAR, 2);
+    }
+
+    public void helper(){
+        char[] charsArray = {'a', 'b', 'c', 'd'};
+        System.out.println(Arrays.toString(this.getBrightness(charsArray)));
+    }
+//    [0.74609375, 0.69140625, 0.8203125, 0.71484375]
+
 }
