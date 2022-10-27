86254d48c9e9adef2f0416e28b4c0b5166aa7356
elilevinkopf <eli.levinkopf@mail.huji.ac.il>
2022-10-27 20:38:41 +0300

    BrightnessImgCharMatcher

--- ex4_1/src/ascii_art/img_to_char/BrightnessImgCharMatcher.java
+++ ex4_1/src/ascii_art/img_to_char/BrightnessImgCharMatcher.java
@@ -20,7 +20,6 @@ public class BrightnessImgCharMatcher {
 
     private float BrightnessImgChar(char c) {
         boolean[][] booleanMatrix = CharRenderer.getImg(c, NUM_OF_PIXELS_FOR_CHAR, "Ariel");
-//        CharRenderer.printBoolArr(booleanMatrix);
         int count = 0;
         for (boolean[] row : booleanMatrix) {
             for (boolean bool : row) {
@@ -32,10 +31,10 @@ public class BrightnessImgCharMatcher {
         return count / (float) Math.pow(NUM_OF_PIXELS_FOR_CHAR, 2);
     }
 
+//    debugging function
     public void helper(){
         char[] charsArray = {'a', 'b', 'c', 'd'};
         System.out.println(Arrays.toString(this.getBrightness(charsArray)));
     }
-//    [0.74609375, 0.69140625, 0.8203125, 0.71484375]
 
 }
